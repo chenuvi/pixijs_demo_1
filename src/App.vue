31a68dea81ea559c1ea69dfd17866d886b9784e3
chenuvi <chenuvi@outlook.com>
2023-08-10 03:06:45 +0800

    Feat:  first

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import * as PIXI from "pixi.js";
+
+// 创建应用
+const app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: '#FA541C',
+  resolution: window.devicePixelRatio || 1,
+});
+
+// 挂载元素
+document.body.appendChild(app.view);
+
+// 创建一个矩形
+const rect = new PIXI.Graphics();
+rect.beginFill(0x66ccff); 
+rect.drawRect(200, 200, 400, 300);
+rect.endFill();
+
+// 矩形添加到舞台
+app.stage.addChild(rect);
+
+
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+canvas {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  right: 0;
+}
+</style>
