aa89370ac57dbf493522a362497844a81b13166b
chenuvi <chenuvi@outlook.com>
2023-08-10 16:33:58 +0800

    Feat: 文字遮罩

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <script setup>
-import * as PIXI from "pixi.js";
+  import * as PIXI from "pixi.js"
+  // 本地图片转 URl
+  function getImageUrl(name, suffix = png) {
+    return new URL(`./assets/${name}.${suffix}`, import.meta.url).href
+  }
 
-// 创建应用
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 'lightblue',
-  resolution: window.devicePixelRatio || 1,
-});
+  // 创建应用
+  const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: "lightblue",
+    resolution: window.devicePixelRatio || 1,
+  })
 
-// 挂载元素
-document.body.appendChild(app.view);
+  // 挂载元素
+  document.body.appendChild(app.view)
 
-// 创建一个矩形
-const rect = new PIXI.Graphics();
-rect.beginFill(0x66ccff);
-rect.drawRect(200, 200, 400, 300);
-rect.endFill();
+  // 显示 hello world
+  const text = new PIXI.Text("hello world", {
+    fontFamily: "Arial",
+    fontSize: 300,
+    fill: "#000000",
+    align: "center",
+  })
+  text.x = app.screen.width / 2
+  text.y = app.screen.height / 2
+  text.anchor.set(0.5)
+  app.stage.addChild(text)
 
-// 矩形添加到舞台
-app.stage.addChild(rect);
+  // 创建一个精灵，并铺满整个屏幕
+  const sprite = PIXI.Sprite.from(getImageUrl("Rick_And_Morty", "jpg"))
+  sprite.width = app.screen.width
+  sprite.height = app.screen.height
+
+  // text 作为 sprite 的遮罩层
+  // TODO sprite.mask = text
+  text.mask = sprite
 
 
+  app.stage.addChild(sprite)
 </script>
 
 <template>
@@ -29,16 +46,16 @@ app.stage.addChild(rect);
 </template>
 
 <style scoped>
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-canvas {
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  left: 0;
-  right: 0;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  canvas {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    right: 0;
+  }
 </style>
