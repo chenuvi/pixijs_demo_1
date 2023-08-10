0555b8633d61192248bc78e2dff3094cd38de48f
chenuvi <chenuvi@outlook.com>
2023-08-10 17:50:12 +0800

    Feat: 轮廓滤镜 发光滤镜

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import * as PIXI from "pixi.js"
+  import { OutlineFilter, GlowFilter } from "pixi-filters"
   // 本地图片转 URl
   function getImageUrl(name, suffix = "png") {
     return new URL(`./assets/${name}.${suffix}`, import.meta.url).href
@@ -16,14 +17,50 @@
   // 挂载元素
   document.body.appendChild(app.view)
 
-  // 创建一个矩形
-  const rect = new PIXI.Graphics()
-  rect.beginFill(0x66ccff)
-  rect.drawRect(200, 200, 400, 300)
-  rect.endFill()
+  // 创建一个纹理
+  const texture = PIXI.Texture.from(getImageUrl("bunny"))
 
-  // 矩形添加到舞台
-  app.stage.addChild(rect)
+  // 创建一个精灵
+  const sprite = new PIXI.Sprite(texture)
+  sprite.anchor.x = 0.5
+  sprite.anchor.y = 0.5
+
+  // 精灵放大两倍
+  sprite.scale.x = 2
+  sprite.scale.y = 2
+
+  sprite.position.x = app.screen.width / 2
+  sprite.position.y = app.screen.height / 2
+  app.stage.addChild(sprite)
+
+  // 创建模糊滤镜
+  const blurFilter = new PIXI.BlurFilter()
+  blurFilter.blur = 3
+
+  // // 监听鼠标移入精灵
+  // sprite.interactive = true
+  // sprite.on("pointerover", () => {
+  //   blurFilter.blur = 0
+  // })
+
+  // // 监听鼠标移出精灵
+  // sprite.on("pointerout", () => {
+  //   blurFilter.blur = 3
+  // })
+
+  // 创建轮廓滤镜
+  const outlineFilter = new OutlineFilter(10, 0xffff00)
+  // 创建发光过滤滤镜
+  const glowFilter = new GlowFilter({
+    color: 0xff0000,
+    alpha: 0.5,
+    distance: 15,
+    quality: 0.5,
+    outerStrength: 10,
+  })
+  glowFilter.color = 0xff0000
+  glowFilter.alpha = 0.5
+  sprite.filters = [outlineFilter, glowFilter]
 </script>
 
 <template>
