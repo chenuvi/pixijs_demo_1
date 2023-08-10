02f0b8621afeaae9a4388b445b0fde73b0e64991
chenuvi <chenuvi@outlook.com>
2023-08-10 16:53:43 +0800

    Feat: add img

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <script setup>
-import * as PIXI from "pixi.js";
+  import * as PIXI from "pixi.js"
+  // 本地图片转 URl
+  function getImageUrl(name, suffix = "png") {
+    return new URL(`./assets/${name}.${suffix}`, import.meta.url).href
+  }
 
-// 创建应用
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 'lightblue',
-  resolution: window.devicePixelRatio || 1,
-});
+  // 创建应用
+  const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: "lightblue",
+    resolution: window.devicePixelRatio || 1,
+  })
 
-// 挂载元素
-document.body.appendChild(app.view);
+  // 挂载元素
+  document.body.appendChild(app.view)
 
-// 创建一个矩形
-const rect = new PIXI.Graphics();
-rect.beginFill(0x66ccff);
-rect.drawRect(200, 200, 400, 300);
-rect.endFill();
+  // 创建一个矩形
+  const rect = new PIXI.Graphics()
+  rect.beginFill(0x66ccff)
+  rect.drawRect(200, 200, 400, 300)
+  rect.endFill()
 
-// 矩形添加到舞台
-app.stage.addChild(rect);
-
-
+  // 矩形添加到舞台
+  app.stage.addChild(rect)
 </script>
 
 <template>
@@ -29,16 +31,16 @@ app.stage.addChild(rect);
 </template>
 
 <style scoped>
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-canvas {
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  left: 0;
-  right: 0;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  canvas {
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    left: 0;
+    right: 0;
+  }
 </style>
